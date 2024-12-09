0ac260d3bb26bb2801db20ad8533c81c930c2bdd
Dr Ed Wallitt <edwallitt@icloud.com>
2024-12-09 19:55:57 +0000

    add tweek form

--- lib/twittex_web/live/profile_live.ex
+++ lib/twittex_web/live/profile_live.ex
@@ -15,4 +15,24 @@ defmodule TwittexWeb.ProfileLive do
     form = Feed.change_tweek(%Tweek{}) |> to_form
     {:ok, assign(socket, form: form, tweeks: tweeks, user: user)}
   end
+
+  def handle_event("save", %{"tweek" => tweek_params}, socket) do
+    current_user = socket.assigns.current_user
+
+    case Feed.create_tweek_for_user(current_user, tweek_params) do
+      {:ok, tweek} ->
+        {:noreply,
+         socket
+         |> update(:tweeks, &[tweek | &1])
+         |> assign(:form, %Tweek{} |> Feed.change_tweek() |> to_form())}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+
+  def handle_event("validate", %{"tweek" => tweek_params}, socket) do
+    form = %Tweek{} |> Feed.change_tweek(tweek_params) |> to_form()
+    {:noreply, assign(socket, :form, form)}
+  end
 end
